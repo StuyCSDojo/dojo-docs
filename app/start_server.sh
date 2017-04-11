b5407d5b4eb8ac0f1b171f32436aedc130da9f57
Patrick Chan <patrickchan1999@gmail.com>
2017-04-11 15:36:35 -0400

    Updated start_server to kill all processes and start all processes

---
+++ app/start_server.sh
@@ -0,0 +1,10 @@
+#! /bin/bash -e
+
+sudo pkill gunicorn
+cd /projects/dojo-docs/app
+gunicorn -w 4 -b unix:/tmp/docs.sock -m 005 api:app --daemon &> /dev/null
+cd /projects/dojo-resources/app
+gunicorn -w 4 -b unix:/tmp/resources.sock -m 005 api:app --daemon &> /dev/null
+cd /projects/dojo-website/app
+gunicorn -w 4 -b unix:/tmp/website.sock -m 005 api:app --daemon &> /dev/null
+
